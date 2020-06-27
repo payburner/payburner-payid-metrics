d1ce4d15f7db8883d7fc569448dd8729ba4c5cd6
Chris Opler <chrisopler@yahoo.com>
2020-06-27 05:18:08 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # payburner-payid-metrics
+
+This project includes the metrics code from from the reference implementation of the payid server,
+allowing you to set metrics in any implementation.
--- src/__tests__/PayIDMetricsService.test.ts
+++ src/__tests__/PayIDMetricsService.test.ts
@@ -1,7 +1,7 @@
 import {PayIDMetricsConfiguration, PayIDMetricsService} from '../index';
 test('Test Default Interval Seconds', () => {
     const metricsConfig = new PayIDMetricsConfiguration('tag1', 'payburner.com');
-    expect(new Number(metricsConfig.payIdCountRefreshIntervalInSeconds).toFixed(0)).toBe("60");
+    expect( Number(metricsConfig.payIdCountRefreshIntervalInSeconds).toFixed(0)).toBe("60");
 });
 
 test('Test Set Value', () => {
